d099c6b3f6d9e1cd1fe3ee9a92059a376971606f
Hieu Do <hieu271101@gmail.com>
2026-05-30 23:12:19 +0700

    Update normalization parameters in evaluation transform

--- evaluate.py
+++ evaluate.py
@@ -284,10 +284,7 @@ def main():
     transform = transforms.Compose([
         transforms.Resize((299, 299)),
         transforms.ToTensor(),
-        transforms.Normalize(
-            [0.485, 0.456, 0.406],
-            [0.229, 0.224, 0.225]
-        )
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
 
     # =====================================================
